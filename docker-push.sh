ab17b24b7ddfba8d7aa4024ec8674672467bac7c
Torello Querci <tquerci@gmail.com>
2019-12-16 22:53:41 +0100

    Fix a typo in docker-push.sh script

--- docker-push.sh
+++ docker-push.sh
@@ -2,4 +2,4 @@
 
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push mk8testing/simple-project:tagname
+docker push mk8testing/simple-project:latest
