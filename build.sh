14f32b8e16b07d21a2cf842059ffe3a4a02cdb18
Torello Querci <tquerci@gmail.com>
2019-12-16 22:48:09 +0100

    Tag build image to push it on DockerHub repository

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Bild source code
+# Build source code
 docker build -t simple-service -f Dockerfile_build .
+
+# Tag image to push on DockerHub repository
+docker tag simple-service mk8testing/simple-service
