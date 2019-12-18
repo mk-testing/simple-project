2c08245efc786850c240cd18fbe1d8ba64b6dd8b
Torello Querci <tquerci@gmail.com>
2019-12-18 12:57:03 +0100

    Local build are now docker-cache indipendent

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Update BUILD_TIMESTAMP to force image recreation
+now=$(date)
+sed "s/<BUILD_TIMESTAMP>/$now/" Dockerfile_build > .Dockerfile_build
+
 # Build source code
-docker build -t simple-service -f Dockerfile_build .
+docker build -t simple-service -f .Dockerfile_build .
--- build_baseimage.sh
+++ build_baseimage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Update BUILD_TIMESTAMP to force image recreation
+now=$(date)
+sed "s/<BUILD_TIMESTAMP>/$now/" Dockerfile_platform > .Dockerfile_platform
+
 # Create Base Image
-docker build -t simple-service-baseimage -f Dockerfile_platform .
+docker build -t simple-service-baseimage -f .Dockerfile_platform .
